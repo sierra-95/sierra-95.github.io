02265ddfb41ddef9a3243b985c6b087dafa30c0f
sierra-95 <michaelmachohi@gmail.com>
2024-03-23 23:32:26 +0300

    Added articles on article section

--- Backup/Assets/js/hamburger-menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
-  const navLinks = document.querySelector('.nav-list');
-
-  hamburgerMenu.addEventListener('click', function () {
-      navLinks.classList.toggle('show');
-  });
-});
